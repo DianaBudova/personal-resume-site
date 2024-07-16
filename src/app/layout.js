bdbce44a129381564ce27f0a3d74d662ea5d4b11
Budova Diana <dianachampions011@gmail.com>
2024-07-16 18:58:07 +0300

    architecture clear

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,12 @@
 // import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import Head from "next/head";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Diana Budova's Portfolio",
-    description: "Diana Budova's portfolio with Next.js",
+    title: "Diana Budova's Resume",
+    description: "Diana Budova's resume with Next.js",
 };
 
 export default function RootLayout({ children }) {
@@ -25,39 +24,7 @@ export default function RootLayout({ children }) {
                     rel="stylesheet"
                 />
             </Head>
-            <html lang="en">
-                {/* <body className={inter.className}> */}
-				<body className="source-code-pro">
-                    <nav className="text-lg sm:text-xl p-4 bg-black bg-opacity-30 backdrop-filter backdrop-blur-sm">
-                        <ul className="flex justify-around">
-                            <li>
-                                <Link href="/">
-                                    <span className="text-white hover:underline">
-                                        About Me
-                                    </span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/projects">
-                                    <span className="text-white hover:underline">
-                                        Projects
-                                    </span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/contact-me">
-                                    <span className="text-white hover:underline">
-                                        Contact Me
-                                    </span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div className="px-7 sm:px-10 md:px-14 py-7">
-                        {children}
-                    </div>
-                </body>
-            </html>
+            <html lang="en">{children}</html>
         </>
     );
 }
