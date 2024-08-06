008383dd4fe9d24b6275fdc359d9df17fbc789b0
Budova Diana <dianachampions011@gmail.com>
2024-08-06 15:54:04 +0300

    deploy prepare 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+    basePath: "/personal-resume-site",
+    output: "export",
     reactStrictMode: true,
 };
 
