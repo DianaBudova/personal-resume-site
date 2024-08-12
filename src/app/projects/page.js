83cbf64687c956c79c9071e7aeb237480b09be41
Budova Diana <dianachampions011@gmail.com>
2024-08-12 20:06:34 +0300

    change title projects

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -19,8 +19,20 @@ const ProjectsComponent = () => {
                         className="flex flex-col justify-between bg-black bg-opacity-20 backdrop-filter rounded-lg shadow-md p-4"
                     >
                         <div className="mb-5">
-                            <h2 className="text-2xl font-black text-center text-zinc-200 mb-2">
-                                {project.title}
+                            <h2 className="text-2xl font-black text-center mb-2">
+                                {project.url && (
+                                    <a
+                                        href={project.url}
+                                        target="_blank"
+                                        className="link-animation text-zinc-200 animate-pulse hover:animate-none"
+                                    >
+                                        {project.title}
+                                    </a>
+                                ) || (
+                                    <span className="text-zinc-200">
+                                        {project.title}
+                                    </span>
+                                )}
                             </h2>
                             <p className="text-zinc-250">
                                 {project.description}
